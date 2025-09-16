98092d7b80851e231967b46a7a7335b49bc47d0c
Alessandro Cordeiro <cordeiroalessandro792@gmail.com>
2025-09-15 21:02:31 -0300

    adicionando segunda activity

--- app/src/main/java/br/com/unifatecie/rmp/MainActivity.java
+++ app/src/main/java/br/com/unifatecie/rmp/MainActivity.java
@@ -1,5 +1,6 @@
 package br.com.unifatecie.rmp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +21,7 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Intent intent = new Intent(getBaseContext(), segunda_Activity.class);// cria a intent para iniciar a segunda activity
+        startActivity(intent);// inicia a segunda activity
     }
 }
---
+++ app/src/main/java/br/com/unifatecie/rmp/SegundaActivity.java
@@ -0,0 +1,11 @@
+package br.com.unifatecie.rmp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SegundaActivity extends Activity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);//
+        setContentView(R.layout.activity_segunda);//carrega o layout
+    }
+}
